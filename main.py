77f76f014d531ac36e0affa824e2c89792294756
Ajesh Sharma <129658242+AjeshCasual@users.noreply.github.com>
2024-09-21 16:03:16 +0530

    commit

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ rl.init_window(800, 450, "EchoExtract")
 rl.gui_load_style("theme.rgs")
 rl.gui_enable()
 while not rl.window_should_close():
-
-    
-
     rl.begin_drawing()
     rl.clear_background(rl.WHITE)
     rl.gui_dummy_rec(rl.Rectangle(10,10,200,200),"etare")
